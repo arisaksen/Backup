28f0b68167fa63e6c2f722f1b09e3602f458fd10
arisaksen <arisaksen@gmail.com>
2017-10-01 13:49:06 +0200

    coping of test directory

--- backup.py
+++ backup.py
@@ -3,14 +3,17 @@
 
 import shutil, os
 from pathlib import Path
+import send2trash
 
-workingDir = str(Path.home()) + '\Desktop' #change for where the files should be copied
+workingDir = str(Path.home()) + '\Test' #change for where the files should be copied
 os.chdir(workingDir)
 
-filesDir = (os.listdir()) #makes a list of all files
-print(filesDir)
+print(workingDir)
 
-# shutil.copy('test.txt', 'C:\\Users\\arisa\\Documents\\test2.txt')		
+if os.path.exists(workingDir+'_Copy'):
+	send2trash.send2trash(workingDir+'_Copy')
+	
+shutil.copytree(workingDir,workingDir+'_Copy')
 
 # try/except around the whole code. Se --> sentex
 # try
