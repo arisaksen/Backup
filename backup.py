6b4a3e4969fd9f2e49df55337f7ab0bda75fe72c
arisaksen <arisaksen@gmail.com>
2017-10-01 12:36:51 +0200

    choosing of directory

--- backup.py
+++ backup.py
@@ -1,4 +1,16 @@
 # script for ziping and backingup files on computer
+#shebang line, read up
+
+import shutil, os
+from pathlib import Path
+
+workingDir = str(Path.home()) + '\Desktop' #change for where the files should be copied
+os.chdir(workingDir)
+
+filesDir = (os.listdir()) #makes a list of all files
+print(filesDir)
+
+# shutil.copy('test.txt', 'C:\\Users\\arisa\\Documents\\test2.txt')		
 
 # try/except around the whole code. Se --> sentex
 # try
